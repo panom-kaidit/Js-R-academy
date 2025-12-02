1ab16b4918db3397e34c75206a7b4f49e852c3c4
Panom M <p.makuei@alustudent.com>
2025-12-02 20:48:41 +0200

    Javascript

---
+++ week 1 day2.js
@@ -0,0 +1,7 @@
+let dealerName = "James"
+let location ="Kampala"
+let product = "Milk"
+
+let message ="Dealer: "+ dealerName + " |Location: " + location +" | Product: "+ product
+
+console.log(message)
